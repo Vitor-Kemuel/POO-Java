b7d3f28daee23e9862ea219244e8a025c3a678d1
Vitor-Kemuel <kemuvitor@gmail.com>
2022-08-04 01:43:46 -0300

    Herança e polimorfismo

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,26 +1,11 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
     private int cargaHoraria;
-
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -32,8 +17,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "curso='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "curso='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,58 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosEscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosEscritos() {
+        return conteudosEscritos;
+    }
+
+    public void setConteudosEscritos(Set<Conteudo> conteudosEscritos) {
+        this.conteudosEscritos = conteudosEscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosEscritos, dev.conteudosEscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosEscritos, conteudosConcluidos);
+    }
+
+    public void escreverBootcamp(Bootcamp bootcamp){
+
+    }
+
+    public void progredir(){
+
+    }
+
+    public void calcularXP(){}
+}
